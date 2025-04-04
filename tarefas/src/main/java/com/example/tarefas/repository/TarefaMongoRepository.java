4b4c00024c5afe556bcd973d21768fc27fb9056d
Kevin Thiago dos Santos <kevinthiago126@gmail.com>
2025-04-03 21:38:48 -0300

    Delete tarefas directory

--- tarefas/src/main/java/com/example/tarefas/repository/TarefaMongoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.tarefas.repository;
-
-import com.example.tarefas.model.TarefaMongo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TarefaMongoRepository extends MongoRepository<TarefaMongo, String> {}
